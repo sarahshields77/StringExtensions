45ca3d3091887746f9c1a9847698e8964d7c99a0
Sarah Shields <sshield9@my.centennialcollege.ca>
2025-03-19 11:24:07 -0400

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace StringExtensions
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpper(this string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                return false;
+
+            return char.IsUpper(value[0]);
+        }
+    }
+}
